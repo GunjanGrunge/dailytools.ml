d3a8dfb886a3efd0f078d533f197277a29f4114f
Gunjan <46925907+GunjanGrunge@users.noreply.github.com>
2023-03-09 15:42:07 +0530

    created config.js
    adding config

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_Q9ubno1bx', 
+        userPoolClientId: '4jvp1rihgi1b2tvejc40l8n4q4', 
+        region: 'ap-south-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
